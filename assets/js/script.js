052e4606567c6a06539eeef8c7c6218a10aedd35
AS <albrecht.stephanie@outlook.fr>
2019-10-28 21:23:55 +0100

    correctif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-function divShow(id,divVisible){
-    if (divVisible == true){
-        // on affiche la div
-        document.getElementById(id).style.display= "block";
-    }
-    else{
-        //on masque la div
-        document.getElementById(id).style.display= "none";
-    }
-}
+var text = document.getElementById('text');
+var links =  document.querySelectorAll('a');//selectionne les liens
+
+links[0].addEventListener('click', function(){//ajoute à l'évènement click la fonction faire apparaitre le texte
+    text.style.visibility = 'visible';
+})
+links[1].addEventListener('click', function(){//masque le texte
+    text.style.visibility = 'hidden';
+})
